b98ef849438951f0a692cb6604b680701a87b731
Parichit Sharma <parichitsharma@outlook.com>
2023-03-12 22:16:36 -0400

    Stable: Fixed minor glitch in DCKMEans

--- run_tests/test_he_data.py
+++ run_tests/test_he_data.py
@@ -15,19 +15,19 @@ Algo2: DCKMeans
 
 # Set parameters
 
-threshold = 0.0
+threshold = 0.001
 num_iterations = 100
 
-file_list = ['test_100_2_3.csv']
+# file_list = ['test_100_2_3.csv']
 
-# file_list = ['magic.csv']
+file_list = ['crop.csv']
 # file_list = ['user_knowledge_train.csv']
 # file_list = ['hapt_train.csv']
 # file_list = ['covertype.csv']
 # file_list = ['spambase.csv']
 # file_list = ['10_clusters.csv']
 
-file_list = ['ringnorm.csv']
+# file_list = ['ringnorm.csv']
 cenfile = 'ringnormCentroids_5_0.txt'
 
 DATA_PATH = "/Users/schmuck/Documents/Box Sync/Ph.D./DATASETS"
@@ -42,7 +42,7 @@ file_path = os.path.join(DATA_PATH, "real_data", "experiment_data", "comma_seper
 # file_path = os.path.join(DATA_PATH, "sample_data")
 
 
-num_clusters = [10]
+num_clusters = [5, 10]
 seed = 1245
 
 seeds = np.random.randint(1, 1200, 1000)
@@ -56,28 +56,24 @@ for data_file in file_list:
     data = read_simulated_data_test(os.path.join(file_path, data_file))
     # centroids1 = read_simulated_data_test(os.path.join(DATA_PATH, "real_data", "experiment_data", "comma_seperated_centroids", cenfile))
     # centroids2 = read_simulated_data_test(os.path.join(DATA_PATH, "real_data", "experiment_data", "comma_seperated_centroids", cenfile))
-    
-    centroids = read_simulated_data_test(os.path.join(DATA_PATH, "real_data", "experiment_data", "test1", cenfile))
-    # print(centroids1)
-    # print(centroids2)
+    # centroids = read_simulated_data_test(os.path.join(DATA_PATH, "real_data", "experiment_data", "test1", cenfile))
 
     print("Data Shape :", data.shape)
-    print(centroids.shape)
 
     for centers in num_clusters:
 
         km_start_time = time.time()
-        km_centroids, km_iter, km_sse, km_calc = Kmeans(data, centers, threshold, num_iterations, centroids, seed)
-        km_TraningTime = round(time.time() - km_start_time, 5)
+        km_centroids, km_iter, km_sse, km_calc = Kmeans(data, centers, threshold, num_iterations, [], seed)
+        km_TraningTime = round(time.time() - km_start_time, 2)
 
         kmdc_start_time = time.time()
-        dckm_centroids, dckm_iter, dckm_sse, dckm_calc = DCKMeans(data, centers, threshold, num_iterations, centroids, seed)
+        dckm_centroids, dckm_iter, dckm_sse, dckm_calc = DCKMeans(data, centers, threshold, num_iterations, [], seed)
         kmdc_TraningTime = round(time.time() - kmdc_start_time, 2)
 
-        print(km_TraningTime, kmdc_TraningTime)
+        print("Total runtime: Kmeans- ", km_TraningTime, "DCKMeans- ", kmdc_TraningTime)
 
-        # print("Distance calculations by KMeans: ", km_calc)
-        # print("Distance calculations by DCKMeans: ", dckm_calc)
+        print("Distance calculations by KMeans: ", km_calc)
+        print("Distance calculations by DCKMeans: ", dckm_calc)
 
         # acc, new_labels1 = calc_raw_accuracy(labels, assign1, data)
         # print("KM: ", acc, check_ARI(new_labels1, labels))
